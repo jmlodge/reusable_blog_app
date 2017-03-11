7bd24236672bd09a769012a854b86a71fe1ba9db
jmlodge <jm-lodge@hotmail.co.uk>
2017-03-11 12:26:38 +0000

    fixes unresolved ref error

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
     README = readme.read()
 
 # allows setup.py to be run from any path
-os.chdir(os.path.normpath(os.join(os.path.abspath(__file__), os.pardir)))
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='reusable-blog-app',
